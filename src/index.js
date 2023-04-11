76ac91696bb508c51769a30bd07ded952ba4172a
Angela <angelakulola4@gmail.com>
2023-04-11 23:28:43 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
 // Code here
+//Defining the server URL
+let pathURL = `http://localhost:3000/beers`
+
+fetchOneBeer();
+// Function that will fetch the first item from the server
+function fetchOneBeer(index = 1) {
+    fetch(`${pathURL}/${index}`)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {console.log(data);
+        const beerName = document.getElementById("beer-name")
+        beerName.textContent = data.name;
+        beerName.dataset.id = data.id;
+        document.getElementById("beer-image").src = data.image_url
+        document.getElementById("beer-description").textContent = data.description
+        
+        //this fetches beer reviews from the server
+        const reviews = data.reviews;
+        console.log(reviews)
+        const reviewList = document.getElementById("review-list");
+        reviewList.innerHTML = "";   
+        reviews.forEach((review) => {
+            const listItem = document.createElement("li");
+            listItem.textContent = review.name;
+            reviewList.appendChild(listItem);
+        });
+    });
+}
+
+
+// Function that will fetch all beers from the server and serve them in a list
+function fetchAllBeers() {
+    fetch(`${pathURL}`)
+   .then((response) => {
+    return response.json();
+   })
+   .then((data) => {
+        const listBeers = document.getElementById("beer-list");
+        listBeers.innerHTML = "";
+        data.forEach(element => {
+            const listItem = document.createElement("li");
+            listItem.textContent = element.name;
+            listBeers.appendChild(listItem);
+        });
+   });
+}
+
+
+fetchAllBeers();
